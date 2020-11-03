2a90c7c507ae7f21805fc73d9f37ad3b69aa54d9
Devan-Kerman <devan@cleverpath.com>
2020-11-02 22:16:00 -0600

    dowward

--- src/quarry/quarry.lua
+++ src/quarry/quarry.lua
@@ -1,22 +1,22 @@
-local maxBlocks = ...
+
 os.loadAPI("quarry/config/ores.lua")
 os.loadAPI("quarry/movement.lua")
 os.loadAPI("quarry/inventory.lua")
 
+local maxBlocks = ...
 function alwaysTrue()
     return true
 end
 
-function start(blocks)
+function drill(blocks)
     if blocks == nil then
         blocks = 100
     end
-    print(string.format("Mining %s blocks!", blocks))
 
     -- keep going!
     local table = {}
     local i = 0
-    while movement.tryForward() do
+    while movement.tryDown() do
         i = i + 1
         recurseVein(table, 0, 0, 0, alwaysTrue, alwaysTrue, alwaysTrue, 0, true)
 
@@ -26,10 +26,8 @@ function start(blocks)
         end
     end
 
-    turtle.turnRight()
-    turtle.turnRight()
     for c = 1, i do
-        movement.tryForward(true)
+        movement.tryUp(true)
     end
 end
 
@@ -45,11 +43,6 @@ function recurseVein(coordinateTable, forward, side, altitude, moveFunction, rev
         if moveFunction(false) then 
             -- try going forward in the vein
             recurseVein(coordinateTable, forward, side, altitude, movement.tryForward, movement.tryBackward, ores.inspectOre, rotation, false)
-            -- then up
-            recurseVein(coordinateTable, forward, side, altitude, movement.tryUp, movement.tryDown, ores.inspectOreUp, rotation, false)
-            -- then down
-            recurseVein(coordinateTable, forward, side, altitude, movement.tryDown, movement.tryUp, ores.inspectOreDown, rotation, false)
-
             -- then right
             turtle.turnRight()
             recurseVein(coordinateTable, forward, side, altitude, movement.tryForward, movement.tryBackward, ores.inspectOre, rotation+1, false)
@@ -83,4 +76,4 @@ function access(coordinateTable, x, z, y)
     return coordinateTable[x][z][y]
 end
 
-start(tonumber(maxBlocks))
+drill(tonumber(maxBlocks))
